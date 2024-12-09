a5083be3c747ae107722eccb996bb8f38dab572b
19a15 <iga1gi500@gmail.com>
2024-12-09 22:59:43 +0400

    Add demo login page

--- MedicalExaminationPreliminaryLists.UI2/Program.cs
+++ MedicalExaminationPreliminaryLists.UI2/Program.cs
@@ -1,6 +1,7 @@
-using Blazored.LocalStorage;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Blazored.LocalStorage;
+
 using MedicalExaminationPreliminaryLists.UI2;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
