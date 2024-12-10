c097c007590e5f0fdd43cc5032ae3f91a16145c4
19a15 <iga1gi500@gmail.com>
2024-12-10 07:41:08 +0400

    Improve UI with role based logic

--- MedicalExaminationPreliminaryLists.UI2/Program.cs
+++ MedicalExaminationPreliminaryLists.UI2/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7112") });
 
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddAuthorizationCore();
